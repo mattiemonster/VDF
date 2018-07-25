155845fa1386b9094324cb25c6cd66ac83164381
Mattie <mattiemonster1@hotmail.com>
2018-07-25 23:19:47 +0100

    Start of VDFExplorer + log class

---
+++ VDFExplorer/Forms/MenuForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using VDFExplorer.Util;
+
+namespace VDFExplorer.Forms
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+            Log.Init();
+            Log.LogInfo("Initialised");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Log.LogInfo("Exiting...");
+            Close();
+        }
+    }
+}
---
+++ VDFExplorer/Util/Log.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace VDFExplorer.Util
+{
+    public class Log
+    {
+        public static FileStream logStream;
+        public static TextWriter tw;
+
+        public static bool closed = false;
+
+        public static void Init()
+        {
+            logStream = new FileStream(GetLogName(), FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            string logPath = logStream.Name;
+            logStream.Close();
+            logStream.Dispose();
+
+            tw = File.CreateText(logStream.Name);
+        }
+
+        public static string GetLogName()
+        {
+            return "log_" + DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year + "_" + 
+                DateTime.Now.Hour + "-" + DateTime.Now.Minute + ".txt";
+        }
+
+        public static string GetDateTimeString()
+        {
+            return DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToShortTimeString() + ":" + 
+                DateTime.Now.Second;
+        }
+
+        public static void LogInfo(string msg)
+        {
+            if (closed)
+                throw new Exception("Tried to log message but the log is closed.");
+
+            tw.WriteLine("[" + GetDateTimeString() + "] INFO: " + msg);
+            tw.Flush();
+        }
+
+        public static void LogWarn(string msg)
+        {
+            if (closed)
+                throw new Exception("Tried to log message but the log is closed.");
+
+            tw.WriteLine("[" + GetDateTimeString() + "] WARN: " + msg);
+            tw.Flush();
+        }
+
+        public static void LogError(string msg)
+        {
+            if (closed)
+                throw new Exception("Tried to log message but the log is closed.");
+
+            tw.WriteLine("[" + GetDateTimeString() + "] ERROR: " + msg);
+            tw.Flush();
+        }
+
+        public static void LogFatal(string msg, bool closeLog)
+        {
+            if (closed)
+                throw new Exception("Tried to log message but the log is closed.");
+
+            tw.WriteLine("[" + GetDateTimeString() + "] FATAL ERROR: " + msg);
+            tw.Flush();
+
+            if (closeLog)
+                Close();
+        }
+
+        public static void Close()
+        {
+            tw.Close();
+            tw.Dispose();
+
+            closed = true;
+        }
+    }
+}
