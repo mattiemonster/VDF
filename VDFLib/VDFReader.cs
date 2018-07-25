1b418fb3172fb83a9d3e9888d519af0153af51eb
Mattie <mattiemonster1@hotmail.com>
2018-07-25 18:00:39 +0100

    VDF Loader

--- VDFLib/VDFReader.cs
+++ VDFLib/VDFReader.cs
@@ -1,10 +1,25 @@
-﻿namespace VDFLib
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace VDFLib
 {
     public class VDFReader
     {
         public static VDF LoadVDF(string path)
         {
-            return new VDF("Temp VDF");
+            VDF vdf;
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+            vdf = (VDF)formatter.Deserialize(stream);
+            stream.Close();
+            return vdf;
+        }
+
+        public static VDF LoadVDF(Stream stream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            return (VDF)formatter.Deserialize(stream);
         }
     }
 }
