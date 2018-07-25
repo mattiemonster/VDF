a2999ebed93b9761c7e6425d066fd4406ae163df
Mattie <mattiemonster1@hotmail.com>
2018-07-25 18:29:20 +0100

    Tests Project Dev

--- VDFConsoleTests/Program.cs
+++ VDFConsoleTests/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VDFLib;
+using VDFLib.Items;
 
 namespace VDFConsoleTests
 {
-    class Program
+    public class Program
     {
+        public string filename;
+
+        static void PrintInfo(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void PrintError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void WaitForKeypress()
+        {
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
+            PrintInfo("VDF Console Tests");
+            WaitForKeypress();
+            PrintError("Error test");
+            WaitForKeypress();
         }
+
     }
 }
